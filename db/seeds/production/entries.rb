b4b5336671327757dd8f6a0713aa8d66b82241ce
yaegashi <yaegashi@xxx-yuki-xxx.local>
2013-04-04 00:12:43 +0900

    db entries

--- db/seeds/production/entries.rb
+++ db/seeds/production/entries.rb
@@ -5,6 +5,6 @@ Entry.create({
   title: "ぴよぴよ開発日記",
   body: "どうも。\nやえです。\n最近はRailsの勉強しています。\n早くRails、あるいはRubyのプログラマです、って自信を持って言えるようになりたい。\nということでとりあえず頑張ります。\n気合いだー！",
   category: "test",
-  posted_at: 10.days.ago.advance,
+  posted_at: Date.new(2013, 1, 1),
   status: draft
 }, without_protection: true)
