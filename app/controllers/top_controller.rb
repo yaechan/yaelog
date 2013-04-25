7aba8837af7e52d8af5ecb969ca05d88b98a2fda
yaegashi <yaegashi@xxx-yuki-xxx.local>
2013-04-25 20:20:21 +0900

    pagination

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-  	@entries = Entry.readable_for(@current_member).order("posted_at DESC").paginate(page: params[:page], per_page: 1)
+  	@entry = Entry.readable_for(@current_member).order("posted_at DESC").first
   end
 
   def about
