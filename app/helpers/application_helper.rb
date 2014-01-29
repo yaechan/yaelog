362583b78ad19c9a5c01b810a3494ac5cab073aa
Yuki Yaegashi <xxx.y.a.e.xxx@me.com>
2014-01-29 21:12:24 +0900

    refactoring
    気が付いたベースでリファクタ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,40 +9,21 @@ module ApplicationHelper
   	link_to_unless_current(text, path) { content_tag(:span, text) }
   end
 
-#  def markdown(text)
-#  	rndr_options = { hard_wrap: true, filter_html: true }
-#  	options = { tables: true,
-#  		        autolink: true,
-#  		        fenced_code_blocks: true,
-#  		        no_intra_emphasis: true }
-#  	rndr = Redcarpet::Render::HTML.new(rndr_options)
-#  	markdown = Redcarpet::Markdown.new(rndr, options)
-#  	syntax_highlighter(markdown.render(text)).html_safe
-#  end
-
-#  def syntax_highlighter(html)
-#  	doc = Nokogiri::HTML(html)
-#  	doc.search("pre").each do |pre|
-#  	  pre.replace Pygments.highlight(pre.text.rstrip, lexer: pre.children.attribute("class").value)
-#  	end
-#  	doc.to_s
-#  end
-
-  def markdown( text )
-    html_render = HtmlWithPygments.new( hard_wrap: true, filter_html: true )
-    markdown = Redcarpet::Markdown.new( html_render,
-                                      autolink: true,
-                                      fenced_code_blocks: true,
-                                      space_after_headers: true,
-                                      tables: true,
-                                      no_intra_emphasis: true,
-                                      strikethrough: true )
-    return markdown.render( text ).html_safe
+  def markdown(text)
+    html_render = HtmlWithPygments.new(hard_wrap: true, filter_html: true)
+    markdown = Redcarpet::Markdown.new(html_render,
+                                       autolink: true,
+                                       fenced_code_blocks: true,
+                                       space_after_headers: true,
+                                       tables: true,
+                                       no_intra_emphasis: true,
+                                       strikethrough: true)
+    return markdown.render(text).html_safe
   end
 
   class HtmlWithPygments < Redcarpet::Render::HTML
-    def block_code( code, language )
-      Pygments.highlight( code, lexer: language, options: { encoding: 'utf-8' } )
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language, options: { encoding: 'utf-8' })
     end
   end
 
